9693457e309d8b9d95b3a372288202dc6466a6e6
hasnain575168 <hasnain575168@gmail.com>
2018-10-08 22:02:18 -0700

    Add and setup views

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const express           = require('express'),
       mongoose          = require('mongoose'),
       bodyParser        = require('body-parser');
 
+// =================================================
+//  DATABASE CONNECTION
+// =================================================
 const dburl = (process.env.DATABASEURL || 'mongodb://localhost/postjot');
 mongoose.connect(dburl)
 .then(db => {
@@ -19,15 +22,25 @@ mongoose.connect(dburl)
     throw err;
 });
 
+// =================================================
+//  MIDDLEWARES
+// =================================================
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
+// =================================================
+//  ROUTES
+// =================================================
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('user/register');
 });
 
+// =================================================
+//  SERVER SETUP
+// =================================================
 app.listen((process.env.PORT || 3000), () => {
     console.log('Server Started at localhost:3000');
 });
