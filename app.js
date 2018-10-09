52bcc04aef0f215c70e9537e438ad5a11ade914c
hasnain575168 <hasnain575168@gmail.com>
2018-10-08 23:13:30 -0700

    implement user register, login, logout

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const express           = require('express'),
       app               = express(),
       path              = require('path'),
       exphbs            = require('express-handlebars'),
+      flash             = require('connect-flash'),
+      session           = require('express-session'),
+      passport          = require('passport'),
       mongoose          = require('mongoose'),
-      bodyParser        = require('body-parser');
+      bodyParser        = require('body-parser'),
+      LocalStrategy     = require('passport-local').Strategy,
+      methodOverride    = require('method-override');
 
 // =================================================
 //  IMPORTING ROUTES
@@ -14,6 +19,9 @@ const express           = require('express'),
 const userRoutes    = require('./routes/user'),
       postRoutes    = require('./routes/post'),
       indexRoutes   = require('./routes/index');
+
+// Requiring User Model
+const User = require('./models/user');
 
 // =================================================
 //  DATABASE CONNECTION
@@ -32,16 +40,46 @@ mongoose.connect(dburl)
 // =================================================
 //  MIDDLEWARES
 // =================================================
+app.use(methodOverride('_method'));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
+// Session Middlewares for user authentication
+app.use(session({
+    secret: 'A Secret Monkey With A Secret Hat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Initializing Flash for Flash Messages
+app.use(flash());
+
+// Initializing passport for user authentication || login/logout
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Setting up local variables
+app.use((req, res, next) => {
+    res.locals.error        = req.flash('error');
+    res.locals.success      = req.flash('success');
+    res.locals.currentUser  = req.user;
+
+    next();
+});
+
 // =================================================
 //  ROUTES
 // =================================================
+// Basic Index Route
 app.use('/', indexRoutes);
+// Post Routes
 app.use('/posts', postRoutes);
+// User Routes
 app.use('/users', userRoutes);
 
 // =================================================
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,52 @@
 const router = require('express').Router();
+const passport = require('passport');
 
+// Importing User Model
+const User = require('../models/user');
+
+// =================================================
+//  REGISTERATION ROUTES
+// =================================================
+router.get('/register', (req, res) => {
+    res.render('user/register');
+});
+
+router.post('/register', (req, res, next) => {
+    let { username, email, password } = req.body;
+
+    const newUser = new User({
+        username,
+        email
+    });
+
+    User.register(newUser, password)
+    .then(user => {
+        console.log(user);
+        res.redirect('/users/login');
+    })
+    .catch(err => {
+        console.log(err);
+        res.redirect('/users/register');
+    });
+});
+
+// =================================================
+//  LOGIN / LOGOUT ROUTES
+// =================================================
 router.get('/login', (req, res) => {
     res.render('user/login');
 });
 
-router.get('/register', (req, res) => {
-    res.render('user/register');
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/posts',
+    failureRedirect: '/users/login'
+}), (req, res) => {
+
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
